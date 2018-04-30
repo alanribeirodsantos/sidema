decf2664a518cc25c98039b25620c2abb261b3f6
Ana Karine <ana_karine02@hotmail.com>
2018-04-30 15:59:27 -0300

    Modelagem de classes

---
+++ src/app/class/organ.ts
@@ -0,0 +1,11 @@
+import { Report } from './report';
+export class Organ{
+    constructor(
+        id: number,
+        name: string,
+        email: string,
+        passoword: string,
+        picture: any,
+        report: Array<Report>,
+    ){}
+}
