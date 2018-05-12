396b84185d56b4d9fd0e44e63745656c269ad7a2
Mateus <mateushcos@gmail.com>
2018-05-12 15:59:59 -0300

    Revisão da Correção

--- src/app/components/sdm-faq-question/sdm-faq-question.component.ts
+++ src/app/components/sdm-faq-question/sdm-faq-question.component.ts
@@ -3,7 +3,8 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'sdm-faq-question',
   templateUrl: './sdm-faq-question.component.html',
-  styleUrls: ['./sdm-faq-question.component.scss']
+  styleUrls: ['./sdm-faq-question.component.scss'],
+  host: {'class': 'uk-width-1-2@m'}
 })
 export class SdmFaqQuestionComponent {
 
