f6176e7fec817b71448eede5889362de7d164c21
Ana Karine <ana_karine02@hotmail.com>
2018-05-13 23:52:50 -0300

    Página Editar Perfil

--- src/app/pages/system/edit-profile/edit-profile.component.ts
+++ src/app/pages/system/edit-profile/edit-profile.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-edit-profile',
+  selector: 'edit-profile',
   templateUrl: './edit-profile.component.html',
   styleUrls: ['./edit-profile.component.scss']
 })
-export class EditProfileComponent implements OnInit {
+export class EditProfileComponent{
+  editProfile(){
 
-  constructor() { }
-
-  ngOnInit() {
   }
-
 }
