842368636876bbbb85f95154bdbbaca628ded9c7
Mateus <31441471+Mathenr@users.noreply.github.com>
2018-05-17 17:07:06 -0300

    Feature/Footer [+ Resolução de Bug no FAQ] (#23)
    * Primeiro commit do footer e correção de bug no FAQ
    
    * Correção de Bugs
    
    * Revisão do PR

---
+++ src/app/components/sdm-footer/sdm-footer.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sdm-footer',
+  templateUrl: './sdm-footer.component.html',
+  styleUrls: ['./sdm-footer.component.scss']
+})
+export class SdmFooterComponent {
+}
