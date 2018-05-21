5857c66cc58cf417dfdd911fb12bf4946156b187
Alan Ribeiro <38020482+alanribeiro@users.noreply.github.com>
2018-05-21 16:14:48 -0300

    Sprint 5 - Editar perfil e Ajustes (#27)
    * Sprint 5 - Editar perfil e Ajustes
    
    * Alterações PR

--- src/app/components/sdm-side-menu/sdm-side-menu.component.ts
+++ src/app/components/sdm-side-menu/sdm-side-menu.component.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { UserService } from '../../backend/services/user/user.service';
 
 @Component({
   selector: 'sdm-side-menu',
   templateUrl: './sdm-side-menu.component.html',
   styleUrls: ['./sdm-side-menu.component.scss']
 })
-export class SdmSideMenuComponent{
+export class SdmSideMenuComponent implements OnInit {
   
-  singOut(){
+  userName:string = "";
 
+  constructor(private userService:UserService) {}
+
+  ngOnInit(){
+    var user = JSON.parse(localStorage.getItem("user"));
+    if(user === null){
+      this.userName = "";
+    }
+    else this.userName = user.name;
   }
-  editProfile(){
-
+  
+  signOut(){
+    this.userService.logout();
   }
 }
