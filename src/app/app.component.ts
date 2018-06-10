ed243386f143c5afe2f206567f1fc1b7b9b86f0e
Mateus <31441471+Mathenr@users.noreply.github.com>
2018-06-10 20:08:43 -0300

    Feature/edu (#50)
    * Mimos
    
    * Responsividade e pequenos detalhes
    
    * Conflitos
    
    * Review

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import * as firebase from 'firebase/app';
+import { Router, NavigationStart, NavigationEnd } from '@angular/router';
+import { Location, PopStateEvent } from "@angular/common";
 
 @Component({
   selector: 'app-root',
@@ -8,7 +10,7 @@ import * as firebase from 'firebase/app';
 })
 export class AppComponent {
 
-  constructor(){
+  constructor(private router: Router, private location: Location){
     var config = {
       apiKey: "AIzaSyASY7GlvEimsp0Gqv1H6qvwdX_w6AI0m6Y",
       authDomain: "sidema-dd795.firebaseapp.com",
@@ -19,4 +21,26 @@ export class AppComponent {
     };
     firebase.initializeApp(config);
   }
+
+  private lastPoppedUrl: string;
+  private yScrollStack: number[] = [];
+
+    ngOnInit() {
+        this.location.subscribe((ev:PopStateEvent) => {
+            this.lastPoppedUrl = ev.url;
+        });
+        this.router.events.subscribe((ev:any) => {
+            if (ev instanceof NavigationStart) {
+                if (ev.url != this.lastPoppedUrl)
+                    this.yScrollStack.push(window.scrollY);
+            } else if (ev instanceof NavigationEnd) {
+                if (ev.url == this.lastPoppedUrl) {
+                    this.lastPoppedUrl = undefined;
+                    window.scrollTo(0, this.yScrollStack.pop());
+                } else {
+                    window.scrollTo(0, 0);
+                }
+            }
+        });
+    }
 }
