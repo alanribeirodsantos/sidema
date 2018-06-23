19b02e3e7f06b55dd60ed7889cccb117ebfb5709
Ana Karine <ana_karine02@hotmail.com>
2018-06-23 18:25:54 -0300

    Textos da seção educacional e ajustes no formulário de denúncia

--- src/app/components/sdm-card-category/sdm-card-category.component.ts
+++ src/app/components/sdm-card-category/sdm-card-category.component.ts
@@ -8,10 +8,10 @@ import { Component } from '@angular/core';
 export class SdmCardCategoryComponent{
 
   categories = [
-    {'img': 'assets/images/category-water-resources.svg', 'category': 'Recursos Hidricos', 'subcategory': ['Poluição de açudes, rios e lagos', 'Construção de poços ilegais', 'Mal uso da água de açudes, rios e lagos'], 'institute': 'SEMA'},
+    {'img': 'assets/images/category-water-resources.svg', 'category': 'Recursos Hidricos', 'subcategory': ['Poluição de açudes, rios e lagos', 'Construção de poços ilegais', 'Mal uso da água de açudes, rios e lagos'], 'institute': 'DNOCS'},
     {'img': 'assets/images/category-monoliths.svg', 'category': 'Monólitos', 'subcategory': ['Depedração e poluição', 'Construções irregulares', 'Apropriação irregular sem mandato'], 'institute': 'SEMA'},
-    {'img': 'assets/images/category-vegetation.svg', 'category': 'Vegetação', 'subcategory': ['Poluição', 'Queimadas', 'Desmatamento irregular sem mandato'], 'institute': 'SEMA'},
-    {'img': 'assets/images/category-historical-patrimony.svg', 'category': 'Patrimônio Historico', 'subcategory': ['Poluição', 'Depedração', 'Ocupação ilegal irregular sem mandato'], 'institute': 'SEMA'}
+    {'img': 'assets/images/category-vegetation.svg', 'category': 'Vegetação', 'subcategory': ['Poluição', 'Queimadas', 'Desmatamento irregular sem mandato'], 'institute': 'TODOS'},
+    {'img': 'assets/images/category-historical-patrimony.svg', 'category': 'Patrimônio Historico', 'subcategory': ['Poluição', 'Depedração', 'Ocupação ilegal irregular sem mandato'], 'institute': 'SEDUMA'}
   ]
   
   constructor() { }
